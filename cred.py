51d27a78c0c060f3bdf6713542efb97871a34aa7
popg <sawdyk123@gmail.com>
2026-08-06 03:36:23 +0100

    Remove dead prototype files; relocate org profile example to docs/

--- cred.py
+++
@@ -1,4 +0,0 @@
-IMAP_SERVER = ''  # IMAP server address for accessing emails
-USERNAME = ''  # Email account username
-PASSWORD = ''  # SMTP password for email account
-vt_api_key = ""  # VirusTotal API key for domain, file and URL scanning
